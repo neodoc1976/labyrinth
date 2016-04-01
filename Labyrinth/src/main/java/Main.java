4cbf9a8532ed753692f2ef4af6eeb771cd2be25d
George <neodoc3.gmail.com>
2016-04-01 22:17:16 +0300

    Interface is added

--- Labyrinth/src/main/java/Main.java
+++ Labyrinth/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Yulya on 01.04.2016.
  */
@@ -5,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Field field = new Field();
         Walker walker = new Walker();
+        Scanner sc = new Scanner(System.in);
         char[][] cells = {
                 {'o', 'o', 'o', 'o', 'o', 'o'},
                 {'o', 'x', 'x', 'o', 'o', 'o'},
@@ -14,9 +17,40 @@ public class Main {
         field.setCells(cells);
         field.setWalker(walker);
         //field.printField();
-        walker.oneRight();
-        walker.oneDown();
-        field.printField();
+        // walker.oneRight();
+        // walker.oneRight();
+        //walker.oneDown();
+        // field.printField();
+        while (true) {
+            System.out.println("Введіть варіант бажаного напрямку кроку термінами 'up','down','right','left':");
+            String state = sc.nextLine();
+            switch (state) {
+                case "up":
+                case "u":
+                    walker.oneUp();
+                    field.printField();
+                    break;
+                case "down":
+                case "d" :
+                    walker.oneDown();
+                    field.printField();
+                    break;
+                case "right":
+                case "r":
+                    walker.oneRight();
+                    field.printField();
+                    break;
+                case "left":
+                case"l":
+                    walker.oneLeft();
+                    field.printField();
+                    break;
+                case "exit":
+                    System.exit(0);
+                default:
+                    System.out.println("Невірний тип команди,повторіть:.");
 
+            }
+        }
     }
 }
